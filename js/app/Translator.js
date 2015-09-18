0d98783ea55cb6f54b67aafa63cfda567fa4af68
Scott Pullen <scottpullen@g.harvard.edu>
2015-09-17 21:27:04 -0400

    moving the rules back into the zombie method, making the views able to be stubbed out

--- js/app/Translator.js
+++ js/app/Translator.js
@@ -1,70 +1,44 @@
 define([], function() {
   var Translator = function() {};
+  /*
+    ????
+    For the translations, do rules run against the running translated text as they're being applied or always against the original text? 
+    For instance, rule 5 is 'i' or 'I' to 'rrRr' and rule 8 is 'R' to 'RR'. If I pass in the text 'i' to the zombify function would 
+    that result in 'rrRr' or 'rrRRr'?
 
-  Translator.prototype.zombify = function(text) {
-    text = this.rule1(text);
-    text = this.rule2(text);
-    text = this.rule3(text);
-    text = this.rule4(text);
-    text = this.rule5(text);
-    text = this.rule6(text);
-    text = this.rule7(text);
-    text = this.rule8(text);
-    text = this.rule9(text);
-    text = this.rule10(text);
-    return text;
-  };
+    Can solve the rule 5 to rule 8 problem by moving rule 8 to after rule 1 below
+
+    ?? OR should one of the made up rules be the previous rules must be applied in order, so they can be unzombified properly
+   */
 
   // 1. lower-case "r" at the end of words replaced with "rh".
-  Translator.prototype.rule1 = function(text) {
-    return text.replace(/r\b/g, 'rh');
-  };
-
   // 2. an "a" or "A" by itself will be replaced with "hra".
-  Translator.prototype.rule2 = function(text) {
-    return text.replace(/\ba\b/gi, 'hra');
-  };
-
   // 3. the starts of sentences are capitalised (the "start of a sentence" is any occurrence of
   //   ".!?", followed by a space, followed by a letter.)
-  Translator.prototype.rule3 = function(text) {
-    // TOOD
+  // 4. "e" or "E" is replaced by "rr"
+  // 5. "i" or "I" is replaced by "rrRr"
+  // 6. "o" or "O" is replaced by "rrrRr"
+  // 7. "u" or "U" is replaced by "rrrrRr"
+  // 8. "R" is replaced by "RR"
+  // 9. "y" or "Y" is replaced by "RRR".
+  // 10. "g" or "G" at beginning of words is replaced by "GaR" 
+  Translator.prototype.zombify = function(text) {
+    text = text.replace(/r\b/g, 'rh');;
+    text = text.replace(/\ba\b/gi, 'hra');
+    text = text.replace(/[.!?] \b\w/g, function(m) { return m.toUpperCase(); });;
+    text = text.replace(/e/gi, 'rr');
+    text = text.replace(/i/gi, 'rrRr');
+    text = text.replace(/o/gi, 'rrrRr');
+    text = text.replace(/u/gi, 'rrrrRr');
+    text = text.replace(/R/g, 'RR');
+    text = text.replace(/y/gi, 'RRR');
+    text = text.replace(/\bg/gi, 'GaR');
     return text;
   };
 
-  // 4. "e" or "E" is replaced by "rr"
-  Translator.prototype.rule4 = function(text) {
-    return text.replace(/e/gi, 'rr');
-  };
-
-  // 5. "i" or "I" is replaced by "rrRr"
-  Translator.prototype.rule5 = function(text) {
-    return text.replace(/i/gi, 'rrRr');
-  };
-
-  // 6. "o" or "O" is replaced by "rrrRr"
-  Translator.prototype.rule6 = function(text) {
-    return text.replace(/o/gi, 'rrrRr');
-  };
-
-  // 7. "u" or "U" is replaced by "rrrrRr"
-  Translator.prototype.rule7 = function(text) {
-    return text.replace(/u/gi, 'rrrrRr');
-  };
-
-  // 8. "R" is replaced by "RR"
-  Translator.prototype.rule8 = function(text) {
-    return text.replace(/R/g, 'RR');
-  };
-
-  // 9. "y" or "Y" is replaced by "RRR".
-  Translator.prototype.rule9 = function(text) {
-    return text.replace(/y/gi, 'RRR');
-  };
-
-  // 10. "g" or "G" at beginning of words is replaced by "GaR"
-  Translator.prototype.rule10 = function(text) {
-    return text.replace(/\bg/gi, text);
+  // TODO
+  Translator.prototype.unzombify = function(text) {
+    return text;
   };
 
   return Translator;
--- js/app/TranslatorView.js
+++ js/app/TranslatorView.js
@@ -1,29 +1,37 @@
 define(['jquery', 'Translator'], function($, Translator) {
-  var TranslatorView = function() {
+  var TranslatorView = function(config) {
+    this.config = config || {};
+
+    this.$englishToZombieBtn = this.config.englishToZombieBtn || $('#english-to-zombie-btn');
+    this.$english = this.config.english || $('#english');
+
+    this.$zombieToEnglishBtn = this.config.zombieToEnglishBtn || $('#zombie-to-english-btn');
+    this.$zombie = this.config.zombie || $('#zombie');
+
     this.translator = new Translator();
     this.bindEngishToZombieEvents();
     this.bindZombieToEnglishEvents();
   }
 
   TranslatorView.prototype.bindEngishToZombieEvents = function() {
-    $('#english-to-zombie-btn').on('click', this.zombify.bind(this));
-    $('#english').on("keyup", this.zombify.bind(this));
+    this.$englishToZombieBtn.on('click', this.zombify.bind(this));
+    this.$english.on("keyup", this.zombify.bind(this));
   };
 
   TranslatorView.prototype.zombify = function(event) {
-    var text = $('#english').val();
-    $('#zombie').val(this.translator.zombify(text));
+    var text = this.$english.val();
+    this.$zombie.val(this.translator.zombify(text));
     return false;
   };
 
   TranslatorView.prototype.bindZombieToEnglishEvents = function() {
-    $('#zombie-to-english-btn').on('click', this.unzombify.bind(this));
-    $('#zombie').on('keyup', this.unzombify.bind(this));
+    this.$zombieToEnglishBtn.on('click', this.unzombify.bind(this));
+    this.$zombie.on('keyup', this.unzombify.bind(this));
   };
 
   TranslatorView.prototype.unzombify = function(event) {
-    var text = $('#zombie').val();
-    $('#english').val(this.translator.unzombify(text));
+    var text = this.$zombie.val();
+    this.$english.val(this.translator.unzombify(text));
     return false;
   };
 
