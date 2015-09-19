057f2af3854dbb84d21ed574de15fabdba7fbcc8
Scott Pullen <scottpullen@g.harvard.edu>
2015-09-19 09:15:14 -0400

    Adding karma test runner

--- js/tests/tests-main.js
+++ js/tests/tests-main.js
@@ -1,3 +1,4 @@
+/*
 (function() {
   'use strict';
 
@@ -40,3 +41,26 @@
     });
   });
 })();
+*/
+
+var tests = [];
+for (var file in window.__karma__.files) {
+  if (window.__karma__.files.hasOwnProperty(file)) {
+    if (/Spec\.js$/.test(file)) {
+      tests.push(file);
+    }
+  }
+}
+
+requirejs.config({
+  // Karma serves files from '/base'
+  baseUrl: '/base/js/app',
+  paths: {
+    'jquery': '../vendors/jquery.min'
+  },
+
+  // ask Require.js to load these files (all our tests)
+  deps: tests,
+
+  callback: window.__karma__.start
+});
