01fa966c3be237119d4e31a73bad15fac568103c
Scott Pullen <scottpullen@g.harvard.edu>
2015-09-24 17:28:48 -0400

    Updating the r to RR rule to skip rh at word boundaries instead of phrase endings

--- js/app/Translator.js
+++ js/app/Translator.js
@@ -15,7 +15,7 @@ define([], function() {
   Translator.prototype.zombify = function(text) {
     text = text.replace(/[.!?] \b\w/g, function(m) { return m.toUpperCase(); }); // rule 3 and rule 10
     text = text.replace(/r\b/g, 'rh'); // rule 1
-    text = text.replace(/(?!(rh$|\brh\b))r/gi, 'RR'); // rule 8
+    text = text.replace(/(?!(rh\b|\brh\b))r/gi, 'RR'); // rule 8
     text = text.replace(/a/gi, 'hra'); // rule 2
     text = text.replace(/e/gi, 'rr'); // rule 4
     text = text.replace(/i/gi, 'rrRr'); // rule 5
@@ -27,6 +27,14 @@ define([], function() {
 
   Translator.prototype.unzombify = function(text) {
     text = text.replace(/\bLLL/g, 'g');
+    text = text.replace(/hra/g, 'a');
+    text = text.replace(/rrrrRr/g, 'u');
+    text = text.replace(/rrrRr/g, 'o');
+    text = text.replace(/rrRr/g, 'i');
+    text = text.replace(/rr/g, 'e');
+    text = text.replace(/rh\b/g, 'r');
+    text = text.replace(/RR/g, 'r');
+
     text = text.replace(/[.!?] \b\w/g, function(m) { return m.toUpperCase(); });
     return text;
   };
